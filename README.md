830bd91c0d3c751d223130294871d69ec3e808cd
lucasfturos <lucasfturos@gmail.com>
2022-08-11 00:04:27 -0300

    Adição da página de Data/Hora

--- README.md
+++ README.md
@@ -7,5 +7,5 @@ Testes dos recursos do GitHub Pages
 ## Por enquanto o site tem:
 - **Uma apresentação;**
 - **Um currículo;**
-- **Dois modelos 3D (uma esfera e uma toróide) feitos em JavaScript, o código dos modelos são mostrados em um botão "Show/Hide Code" para quem quiser copiar o código e testar nas suas aplicações WEB.**
-
+- **Dois modelos 3D (uma esfera e uma toróide) feitos em JavaScript**
+  - O código dos modelos são mostrados em um botão "Show/Hide Code" para quem quiser copiar o código e testar nas suas aplicações WEB.
---
+++ js/data_hora.js
@@ -0,0 +1,32 @@
+const currentTime = () => {
+    const elementHora = document.getElementById('hora')
+
+    let date = new Date()
+    let hh = date.getHours()
+    let mm = date.getMinutes()
+    let ss = date.getSeconds()
+
+    hh = hh < 10 ? `0${hh}` : hh
+    mm = mm < 10 ? `0${mm}` : mm
+    ss = ss < 10 ? `0${ss}` : ss
+
+    let time = `${hh}:${mm}:${ss}`
+    elementHora.innerText = time
+}
+
+const currentDate = () => {
+    const elementData = document.getElementById('data')
+
+    let date = new Date()
+    let year = date.getFullYear()
+    let month = date.getMonth() + 1
+    let day = date.getDate()
+
+    month = month < 10 ? `0${month}` : month
+
+    elementData.innerHTML = day + "/" + `${month}` + "/" + year
+}
+
+currentDate()
+currentTime()
+setInterval(currentTime, 1000)
