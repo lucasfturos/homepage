b9d2a87069948b1e0234bf162e574101bf7ed050
lucasfturos <lucasfturos@gmail.com>
2025-02-04 19:23:55 -0300

    Usando o método de localStorage para guardar as voltas

--- _assets/js/chronometer.js
+++ _assets/js/chronometer.js
@@ -1,3 +1,5 @@
+const MAX_STORAGE_SIZE = 4096; // 32 voltas
+
 let timer;
 let isRunning = false;
 let timeElapsed = 0;
@@ -22,6 +24,30 @@ const updateDisplay = () => {
         .padStart(2, "0")}<span class="milliseconds">.${milliseconds
         .toString()
         .padStart(2, "0")}</span>`;
+};
+
+const getStorageSize = () => {
+    let totalSize = 0;
+    for (let key in localStorage) {
+        if (localStorage.hasOwnProperty(key)) {
+            totalSize += new Blob([localStorage[key]]).size;
+        }
+    }
+    return totalSize;
+};
+
+const loadLaps = () => {
+    const savedLaps = JSON.parse(localStorage.getItem("laps")) || [];
+    savedLaps.forEach((lap, index) => {
+        let row = document.createElement("tr");
+        row.innerHTML = `
+            <td>${index + 1}</td>
+            <td>${lap.formattedLapTime}</td>
+            <td>${lap.formattedTotalTime}</td>
+        `;
+        lapsBody.insertBefore(row, lapsBody.firstChild);
+    });
+    tableContainer.style.display = savedLaps.length > 0 ? "block" : "none";
 };
 
 const startChronometer = () => {
@@ -61,6 +87,7 @@ const resetChronometer = () => {
     tableContainer.style.display = "none";
 
     lapsBody.innerHTML = "";
+    localStorage.removeItem("laps");
 };
 
 const addLap = () => {
@@ -90,13 +117,31 @@ const addLap = () => {
     `;
 
     lapsBody.insertBefore(row, lapsBody.firstChild);
-
     tableContainer.style.display = "block";
-
     lapTime = 0;
+
+    const savedLaps = JSON.parse(localStorage.getItem("laps")) || [];
+    savedLaps.push({
+        formattedLapTime: formattedTime,
+        formattedTotalTime: totalFormattedTime,
+    });
+
+    const newStorageSize = getStorageSize();
+    if (
+        newStorageSize + new Blob([JSON.stringify(savedLaps)]).size <=
+        MAX_STORAGE_SIZE
+    ) {
+        localStorage.setItem("laps", JSON.stringify(savedLaps));
+    } else {
+        alert(
+            "O limite de armazenamento foi atingido. Não é possível adicionar mais voltas."
+        );
+    }
 };
 
 startBtn.addEventListener("click", startChronometer);
 stopBtn.addEventListener("click", stopChronometer);
 refreshBtn.addEventListener("click", resetChronometer);
 lapBtn.addEventListener("click", addLap);
+
+loadLaps();
