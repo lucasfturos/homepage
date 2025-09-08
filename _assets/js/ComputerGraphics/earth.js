5fd621c44dce5154e2d8e23e25960a11b71c8463
lucasfturos <lucasfturos@gmail.com>
2025-09-07 23:31:04 -0300

    Correção de textura, luz e inclinação

--- _assets/js/ComputerGraphics/earth.js
+++ _assets/js/ComputerGraphics/earth.js
@@ -27,9 +27,17 @@ const cloudsTexture = loader.load(
     "https://raw.githubusercontent.com/lucasfturos/myhomepage-image/refs/heads/main/img/computerGraphics/earth_clouds.jpg"
 );
 
+dayTexture.wrapS = dayTexture.wrapT = THREE.RepeatWrapping;
+nightTexture.wrapS = nightTexture.wrapT = THREE.RepeatWrapping;
+cloudsTexture.wrapS = cloudsTexture.wrapT = THREE.RepeatWrapping;
+
+dayTexture.minFilter = dayTexture.magFilter = THREE.LinearFilter;
+nightTexture.minFilter = nightTexture.magFilter = THREE.LinearFilter;
+cloudsTexture.minFilter = cloudsTexture.magFilter = THREE.LinearFilter;
+
 const earthGeometry = new THREE.SphereGeometry(1, 64, 64);
 
-let earthRotation = 0;
+const axialTilt = 23.5 * Math.PI / 180;
 
 const earthMaterial = new THREE.ShaderMaterial({
     uniforms: {
@@ -37,10 +45,10 @@ const earthMaterial = new THREE.ShaderMaterial({
         nightTex: { value: nightTexture },
         sunDir: { value: new THREE.Vector3(1, 0, 0) },
         rotation: { value: 0 },
+        axialTilt: { value: axialTilt },
     },
     vertexShader: `
         varying vec3 vNormal;
-        
         void main() {
             vNormal = normalize(normalMatrix * normal);
             gl_Position = projectionMatrix * modelViewMatrix * vec4(position,1.0);
@@ -51,22 +59,32 @@ const earthMaterial = new THREE.ShaderMaterial({
         uniform sampler2D nightTex;
         uniform vec3 sunDir;
         uniform float rotation;
+        uniform float axialTilt;
 
         varying vec3 vNormal;
 
         const float PI = ${Math.PI};
         const float TWO_PI = 2.0 * PI;
 
+        vec3 rotateY(vec3 v, float angle) {
+            float c = cos(angle);
+            float s = sin(angle);
+            return vec3(c * v.x + s * v.z, v.y, -s * v.x + c * v.z);
+        }
+
+        vec3 rotateX(vec3 v, float angle) {
+            float c = cos(angle);
+            float s = sin(angle);
+            return vec3(v.x, c * v.y - s * v.z, s * v.y + c * v.z);
+        }
+
         void main() {
-            float cosR = cos(rotation);
-            float sinR = sin(rotation);
             vec3 n = vNormal;
-            vec3 rotatedNormal =
-                vec3(cosR * n.x + sinR * n.z, n.y, -sinR * n.x + cosR * n.z);
-            float dotLN = dot(normalize(rotatedNormal), normalize(sunDir));
+            n = rotateY(n, rotation);
+            n = rotateX(n, axialTilt);
+            float dotLN = dot(normalize(n), normalize(sunDir));
             vec2 uv =
-                vec2(1.0 - (atan(rotatedNormal.z, rotatedNormal.x) / TWO_PI + 0.5),
-                    (asin(rotatedNormal.y) / PI + 0.5));
+                vec2(1.0 - (atan(n.z, n.x) / TWO_PI + 0.5), (asin(n.y) / PI + 0.5));
             vec4 dayColor = texture2D(dayTex, uv);
             vec4 nightColor = texture2D(nightTex, uv);
             float factor = clamp(dotLN, 0.0, 1.0);
@@ -82,11 +100,11 @@ const cloudsMaterial = new THREE.ShaderMaterial({
     uniforms: {
         cloudsTex: { value: cloudsTexture },
         rotation: { value: 0 },
+        axialTilt: { value: axialTilt },
         opacity: { value: 0.2 },
     },
     vertexShader: `
         varying vec3 vNormal;
-
         void main() {
             vNormal = normalize(normalMatrix * normal);
             gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
@@ -95,6 +113,7 @@ const cloudsMaterial = new THREE.ShaderMaterial({
     fragmentShader: `
         uniform sampler2D cloudsTex;
         uniform float rotation;
+        uniform float axialTilt;
         uniform float opacity;
 
         varying vec3 vNormal;
@@ -102,15 +121,24 @@ const cloudsMaterial = new THREE.ShaderMaterial({
         const float PI = ${Math.PI};
         const float TWO_PI = 2.0 * PI;
 
+        vec3 rotateY(vec3 v, float angle) {
+            float c = cos(angle);
+            float s = sin(angle);
+            return vec3(c * v.x + s * v.z, v.y, -s * v.x + c * v.z);
+        }
+
+        vec3 rotateX(vec3 v, float angle) {
+            float c = cos(angle);
+            float s = sin(angle);
+            return vec3(v.x, c * v.y - s * v.z, s * v.y + c * v.z);
+        }
+
         void main() {
-            float cosR = cos(rotation);
-            float sinR = sin(rotation);
             vec3 n = vNormal;
-            vec3 rotatedNormal =
-                vec3(cosR * n.x + sinR * n.z, n.y, -sinR * n.x + cosR * n.z);
+            n = rotateY(n, rotation);
+            n = rotateX(n, axialTilt);
             vec2 uv =
-                vec2(1.0 - (atan(rotatedNormal.z, rotatedNormal.x) / TWO_PI + 0.5),
-                    (asin(rotatedNormal.y) / PI + 0.5));
+                vec2(1.0 - (atan(n.z, n.x) / TWO_PI + 0.5), (asin(n.y) / PI + 0.5));
             vec4 cloudColor = texture2D(cloudsTex, uv);
             gl_FragColor = vec4(cloudColor.rgb, cloudColor.a * opacity);
         }
@@ -130,18 +158,23 @@ function updateSunDirection() {
         now.getUTCHours() +
         now.getUTCMinutes() / 60 +
         now.getUTCSeconds() / 3600;
-    const angle = (hours / 24) * Math.PI * 2 - Math.PI / 2;
+    const solarLongitude = (hours / 24) * Math.PI * 2 - Math.PI;
     earthMaterial.uniforms.sunDir.value
-        .set(Math.sin(angle), 0, Math.cos(angle))
+        .set(Math.cos(solarLongitude), 0, Math.sin(solarLongitude))
         .normalize();
 }
-updateSunDirection();
-setInterval(updateSunDirection, 1000);
+
+let animationRotation = 0;
 
 function animate() {
-    earthRotation += 0.01;
-    earthMaterial.uniforms.rotation.value = earthRotation;
-    cloudsMaterial.uniforms.rotation.value = earthRotation * 0.6;
+    updateSunDirection();
+    animationRotation += 0.005;
+    earth.rotation.y = animationRotation;
+    clouds.rotation.y = animationRotation;
+
+    earthMaterial.uniforms.rotation.value = earth.rotation.y;
+    cloudsMaterial.uniforms.rotation.value = clouds.rotation.y;
+
     renderer.render(scene, camera);
 }
 
